fabbf11cc9ef1bb1d8c3266d99f97b2e18a923ab
zjw97 <1018510885@qq.com>
2021-05-28 16:04:07 +0800

    update web page and fliter

--- demo/gaokao/views.py
+++ demo/gaokao/views.py
@@ -7,17 +7,41 @@ def search_school(request):
     dic = {}   #字典
     lit = []   #列表，其中包含数个字典
     if request.POST:
-        inp = request.POST.get("q")  # 获得输入
-        # reg = request.POST.get("region")  # 获得输入
-        schools = models.ProfessInformation.objects.filter(pschool__contains=inp)  #检索符合条件的结果
-        # typ = request.POST.get("type")
-        # if typ:
-        #     books = books.filter(btype=typ)
+        conditional = {}
+        # 获取用户提供的学校名称关键字
+        school = request.POST.get("school")
+        conditional['pschool__contains'] = school  # 设置对学校名称关键字的筛选条件
+
+        # 获取用户输入的专业关键
+        profession = request.POST.get("profession")
+        conditional['pprofess_name__contains'] = profession
+
+        # 获取用户输入的分数线下限
+        lscore = request.POST.get("lscore")
+        conditional['pscore_line__gte'] = lscore
+
+        # 获取用户输入的分数线上限
+        hscore = request.POST.get("hscore")
+        conditional['pscore_line__lte'] = hscore
+
+        # 获取用户输入的位次下限值
+        lrank = request.POST.get("lrank")
+        conditional['prank__gte'] = lrank
+
+        # 获取用户输入的位次上限值
+        hrank = request.POST.get("hrank")
+        conditional['prank__lte'] = hrank
+
+        # 获取用户输入的对地区的筛选要求
+        region = request.POST.get("region")
+        conditional['parea__contains'] = region
+        schools = models.ProfessInformation.objects.filter(**conditional)  #检索符合条件的结果, 这里用**可以将字典中的值都取出来
         for i in range(schools.count()):  #对各项信息分类后放入列表
             dic["school"] = schools[i].pschool
             dic["profess_name"]  = schools[i].pprofess_name
             dic["score_line"] = schools[i].pscore_line
             dic["rank"] = schools[i].prank
+            dic["remark"] = schools[i].premark
             lit.append(dic)
             dic = {}
     data["school"] = lit #将变量全部放入data
